0a5aa7398ce3004dd69d82d8322169996730aa2e
NdekoCode <arickbulakali@gmail.com>
2025-05-21 21:47:21 +0200

    feat(api): implement basic authentication module
    This commit introduces a basic authentication module with signup functionality.
    
    - Creates `AuthModule`, `AuthController`, and `AuthService`.
    - Adds a signup route to `AuthController`.
    - Installs `ejs` for view rendering.
    - Configures the application to use `ejs` as the view engine and sets up static assets.
    - Includes `AuthModule` in the main application module.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,18 @@
 import * as dotenv from 'dotenv';
+import { dirname, resolve } from 'path';
 
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './app.module';
 
 dotenv.config();
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const baseDir = dirname(__dirname);
+  app.setBaseViewsDir(resolve(baseDir, 'views'));
+  app.useStaticAssets(resolve(baseDir, 'public'));
+  app.setViewEngine('ejs');
   const PORT = process.env.PORT;
   await app.listen(PORT!, () => {
     console.log(`The app is listening on http://localhost:${PORT}`);
